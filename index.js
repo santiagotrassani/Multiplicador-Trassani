689c29165d11f3801e5da6bf25d169d79895fe95
santiago trassani <santiagotrassani@gmail.com>
2022-08-11 23:44:25 -0300

    primer tp

---
+++ index.js
@@ -0,0 +1,8 @@
+
+let numero = parseInt(prompt("Ingrese un numero del 1 a 12: "));
+let multiplicacion
+
+for(let i = 1; i <= 10 ; i++) {
+    multiplicacion = numero * i
+    alert("Su numero a multipliciar fue: " + numero + "\n" + "==========================" + "\n" + "Tabla de multiplicar:" + "\n" + numero + "X" + i + "=" + multiplicacion )
+};
